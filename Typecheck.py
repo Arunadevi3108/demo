6b9239c107e4bc2222d10d9d51b472728849280d
Arunadevi3108 <114629568+Arunadevi3108@users.noreply.github.com>
2023-05-19 06:26:18 +0530

    Created using Colaboratory

---
+++ Typecheck.py
@@ -0,0 +1,57 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOlt/7fgeVwbLQDINyTe+eK",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Arunadevi3108/demo/blob/master/Typecheck.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "id": "5Vm4rmClTu_h",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "outputId": "af5e581b-7b00-4a21-c8c9-8ece0a0708fd"
+      },
+      "outputs": [
+        {
+          "output_type": "execute_result",
+          "data": {
+            "text/plain": [
+              "str"
+            ]
+          },
+          "metadata": {},
+          "execution_count": 2
+        }
+      ],
+      "source": [
+        "a ='aruna'\n",
+        "type(a)"
+      ]
+    }
+  ]
+}
